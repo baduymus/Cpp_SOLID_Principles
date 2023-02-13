71814b7a7624898977da06846a6aa4cf56515417
Batuhan Arslan <arslanbatu97@gmail.com>
2023-02-14 01:36:34 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class cShape{
+public:
+    int ShapeArea;
+    virtual double calculateArea() = 0;
+};
+
+class cSquare : public cShape{
+private:
+    double sideLength;
+public:
+    cSquare(double side) : sideLength(side){}
+    double calculateArea(){
+        return sideLength * sideLength;
+    }
+};
+
+class cCircle : public cShape {
+private:
+    double radius;
+
+public:
+    cCircle(double r) : radius(r) {}
+    double calculateArea() {
+        return 3.14159265 * radius * radius;
+    }
+};
+
+
+int main(){
+
+    cSquare Square1(5);
+    cCircle Circle1(3);
+
+    cShape* shapes[2];
+    shapes[0] = &Square1;
+    shapes[1] = &Circle1;
+
+    for(int i = 0; i < 2; i++){
+        std::cout << "Area of shape " << (i + 1) << ": "
+                << shapes[i]->calculateArea() << std::endl;
+    }
+
+    return 0;
+
+}
